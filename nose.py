63bdec6f000f6eaa42ea11236793067a9e15ef8e
Andreas Jacobsen <andreas1300@gmail.com>
2018-02-21 18:28:59 +0100

    la til enda flere kommentarer

--- nose.py
+++ nose.py
@@ -20,11 +20,11 @@ while True:
     ret, frame = cap.read()
     # Re-size bassert på ds_factor variabelen over
     frame = cv2.resize(frame, None, fx=ds_factor, fy=ds_factor, interpolation=cv2.INTER_AREA)
-    # Tror dette viser bildet i grått
-    # TODO: Sjekk hva gray faktisk gjør, se om vi kan endre på den
+    # face detectoren kjører bare med sort hvit bilder, så vi konverterer til grayscale her
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     nose_rects = nose_cascade.detectMultiScale(gray, 1.3, 5)
+    # tegner en firkant rundt nesa 
     for (x,y,w,h) in nose_rects:
         cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3)
         break
