6dcb8ff6528c554d1bc1a295ac7ab8f9862f95af
Andreas Jacobsen <andreas1300@gmail.com>
2018-02-21 18:11:20 +0100

    lagt til kommentarer, økt størrelse på bildet fra 0.5 til 1.o scaling. det lagde lagg

--- nose.py
+++ nose.py
@@ -8,12 +8,20 @@ nose_cascade = cv2.CascadeClassifier('/home/andreas/PycharmProjects/learnOpen/ha
 if nose_cascade.empty():
   raise IOError('Unable to load the nose cascade classifier xml file')
 
+# Starter video capture, 0-en viser hvilket kamera programmet skal bruke.
+# Bruker vi ett USB kamera kan IDen være noe annet
 cap = cv2.VideoCapture(0)
-ds_factor = 0.5
+# Faktor definerer størrelsen i faktorer, hva nå enn en faktor er
+ds_factor = 1
 
+# Starter en evig løkke som fortsettere å fange frames helt til vi kaller Esc-knappen (27)
 while True:
+    # Fang nåværende frames
     ret, frame = cap.read()
+    # Re-size bassert på ds_factor variabelen over
     frame = cv2.resize(frame, None, fx=ds_factor, fy=ds_factor, interpolation=cv2.INTER_AREA)
+    # Tror dette viser bildet i grått
+    # TODO: Sjekk hva gray faktisk gjør, se om vi kan endre på den
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     nose_rects = nose_cascade.detectMultiScale(gray, 1.3, 5)
@@ -22,10 +30,13 @@ while True:
         break
 
     cv2.imshow('Nose Detector', frame)
-
+# venter 1 millisekund før den fanger den neste framen
     c = cv2.waitKey(1)
+# Lukker programmet om vi klikker på esc knappen, wait i 1 millisekund (sjekk dette) før vi kan lukke programmet
+
     if c == 27:
         break
-
+# Slipp kamera fri s så du kan bruke det til andre ting
 cap.release()
+# SKru av programmet
 cv2.destroyAllWindows()
