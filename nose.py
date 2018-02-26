d7200802264b49854e3d5d3c08ddd0bc1adce82a
Andreas Jacobsen <andreas1300@gmail.com>
2018-02-26 17:19:26 +0100

    Changed nose rects to rects to fint with the other loops

--- nose.py
+++ nose.py
@@ -51,13 +51,13 @@ while True:
     frame = cv2.resize(frame, None, fx=ds_factor, fy=ds_factor, interpolation=cv2.INTER_AREA)
     # Since the face-detector only works on balck and white images we do a convertion to BGR2GRAY here.
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    nose_rects = nose_cascade.detectMultiScale(gray, 1.3, 5)
+    nose_rect = nose_cascade.detectMultiScale(gray, 1.3, 5)
     # Here we draw the square around the nose that is detected.
     x_dim, y_dim = m.screen_size()
     face_rect = face_cascade.detectMultiScale(gray, 1.3, 5)
     eye_rect = eye_cascade.detectMultiScale(gray, 1.3, 5)
     # Here we draw the square around the nose, face and eyes that is detected.
-    for (x,y,w,h) in nose_rects:
+    for (x,y,w,h) in nose_rect:
         cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255), 3)
         break
     for (x,y,w,h) in face_rect:
