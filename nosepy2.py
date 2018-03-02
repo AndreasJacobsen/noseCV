8fa45394b5f6ffa616feb99e94d06a8c84d45172
Kristian Munter Simonsen <Mokkatroffen@gmail.com>
2018-03-02 21:18:35 +0100

    Tried some different facotrs on cursor. No perma solution.
    Also created a nosepy2 for my own amusement

--- nosepy2.py
+++ nosepy2.py
@@ -61,7 +61,7 @@ m = PyMouse()
 cap = cv2.VideoCapture(0)
 # Display_factor is set to 1 to keep the original size of the captured image.. (Might be an issue if screen is lower ress than webcam).
 # This factor will be used further down in the frame variable that contains cv2.resize.
-ds_factor = 2
+ds_factor = 1
 c = cv2.waitKey(33) & 0xFF
 #  Here we start a loop that will countinue uintill the Esc-button is pushed (27).
 while True:
@@ -78,47 +78,37 @@ while True:
     x_dim, y_dim = m.screen_size()
     nose_rect = nose_cascade.detectMultiScale(gray, 1.3, 5)
     face_rect = face_cascade.detectMultiScale(gray, 1.3, 5)
-    eye_rect = eye_cascade.detectMultiScale(gray, 1.3, 5)
+    #eye_rect = eye_cascade.detectMultiScale(gray, 1.3, 5)
     #smile_rect = smile_cascade.detectMultiScale(gray,1.3, 5)
     #hpalm_rect = palm_cascade.detectMultiScale(gray,1.3,1)
     # Here we draw the square around the nose, face and eyes that is detected.
     # First we check if nose_rect contains any data
     # if not we use the face for mouse navigation instead
 
-
-    if len(nose_rect) >= 0:
-        if cv2.waitKey(1) & 0xFF == ord('q'):  # exit on pressing 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # exit on pressing 'q'
             cap.release()
             cv2.destroyAllWindows()
             break
-        # print ("Detecting nose at ",nose_rect, " using nose to move the mouse")
-        if len(nose_rect) == 1:
-            for (x, y, w, h) in nose_rect:
-
-                cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255), 2)
+    for (x, y, w, h) in face_rect:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        regionface = gray[y:y+h, x:x+w]
+        regioncolor = frame[y:y+h, x:x+w]
+        nose = nose_cascade.detectMultiScale(regionface)
+        for (nx, ny, nw, nh) in nose_rect:
+                cv2.rectangle(regioncolor, (nx, ny), (nx+nw, ny+nh), (0, 255, 0), 2)
                 # Here we say that m (the variable created before, should move the mouse using the x, and y variable from the nose rect.
                 # We have set the variable times 3 as to make the cursor start aprox in the middel.
-                # This formula used is "normal" distance between fgce and screen and with nose in the middle of cam. Adjust acordingly!
-                m.move(x*12-4000, y*4-1000) # Got to find or create  a formula that fits a spesific distance and centering of the face
-                print(x*12-4000, y*4-1000) #Or we need to find something that can limit the frame we capture nose and then use that data for cursor movement.
-
+                m.move(x*8, y*8)
                 if cv2.waitKey(1) == 32:
-                    m.click(x*12-4000, y*4-1000, 2)
+                    m.click(x*8, y*8, 2)
                     print("Click!")
                 # if(len(palm_rect)>0):
                 #      m.click(x,y,1)
                     break
-    elif len(face_rect) > 0:
-        print("No nose detected, falling back to face at coordinates ",face_rect)
-        for (x,y,w,h) in face_rect:
-            cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
-            m.move(x*12-4000, y*4-1000)
 
-    else:
-        print ("Nothing detected, can't controll mouse using nose or face.")
     cv2.imshow('Nesehorn deteksjonsprogram', frame)
     time.sleep(0.005)  # Waiting 1 millisecond to show the next frame.
 # Here we release the webcam to be used by other programs before we shut down the program.
 cap.release()
-# Terminating the window the software is running in. 
+# Terminating the window the software is running in.
 cv2.destroyAllWindows()
