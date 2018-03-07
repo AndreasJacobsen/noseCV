7db8bfa3fb30ee3871d6aaa8b848bb4e363754b3
Kristian Munter Simonsen <Mokkatroffen@gmail.com>
2018-03-07 16:12:07 +0100

    Changes to try and limit the nose detection

--- nose.py
+++ nose.py
@@ -62,17 +62,23 @@ while True:
     # Since the face-detector only works on black and white images we do a conversion to BGR2GRAY here.
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     nose_rect = nose_cascade.detectMultiScale(gray, 1.3, 5)
-    x_dim, y_dim = m.screen_size()
+    w1 = cap.get(3)-60
+    h1 = cap.get(4)-60
+    w_dim, h_dim = m.screen_size()
     face_rect = face_cascade.detectMultiScale(gray, 1.3, 5)
     # Here we draw the square around the nose, face and nose that is detected.
     # If we find something in nose_rect we start a loop to draw any nose that is found.
     if len(nose_rect) > 0:
         print("Only Nose at ", nose_rect)
+
         for (x, y, w, h) in nose_rect:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 3)
+
+            cv2.rectangle(frame, (x, y), (w+x, y+h), (0, 0, 255), 3)
+            x1 = x + 30
+            y1 = y + 30
             # m (the variable created before), should move the mouse using the x, and y variable from the nose rect.
             # We accelerate movement speed by 4 to make it possible to navigate the cursor through the whole screen.
-            m.move(x * 4, y * 4)
+            m.move((x1*w_dim)/w1, (y1*h_dim)/h1)
             # Here we listen for a click of R button or L button to right-click or left-click.
             # The 3D-printed buttons have been mapped to R and L key.
             if cv2.waitKey(1) & 0xFF == ord('r'):
